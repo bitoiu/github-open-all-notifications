68775a2009b09ed96a2bd1a107f4e01a6da00a83
hitode909 <hitode909@gmail.com>
2020-04-29 01:00:51 +0900

    Support new notifications page (#3)
    * Fix selectors
    
    * Fix matches

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,15 +4,15 @@ chrome.extension.sendMessage({}, function (response) {
       clearInterval(readyStateCheckInterval);
 
       console.log("Hello. This message was sent from scripts/inject.js");
-      d3.select('.notification-center .tabnav .float-right')
+      d3.select('.notifications-list .Box-header')
         .insert('a',':first-child')
         .text('Open all')
         .attr('class','btn btn-sm')
         .on("click", function() {
-          d3.selectAll('.notifications-list .list-group-item.unread')
-            .classed('unread',false)
-            .classed('read',true)
-            .selectAll('a.list-group-item-link')
+          d3.selectAll('.notification-unread')
+            .classed('notification-unread',false)
+            .classed('notification-read',true)
+            .selectAll('a.notification-list-item-link')
             .each(function() {
               window.open(this.href);
             })
